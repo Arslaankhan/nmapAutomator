570461e7f3a17bbad0700f43bc348398a7f86990
Rana <rana@Ranas-MacBook-Air.local>
2020-01-14 22:28:21 -0500

    changed gobuster wordlist to medium and added file extensions

--- nmapAutomator.sh
+++ nmapAutomator.sh
@@ -333,17 +333,17 @@ for line in $file; do
 	if [[ ! -z $(echo "${line}" | grep -i http) ]]; then
 		port=$(echo "${line}" | cut -d "/" -f 1)
 		if [[ ! -z $(echo "${line}" | grep -w "IIS") ]]; then
-			pages=".html,.asp,.php"
+			pages=".html,.asp,.php,.txt, .sh, .cgi"
 		else
-			pages=".html,.php"
+			pages=".html,.php, .txt, .sh, .cgi"
 		fi
 		if [[ ! -z $(echo "${line}" | grep ssl/http) ]]; then
 			#echo "sslyze --regular $1 | tee recon/sslyze_$1_$port.txt"
 			echo "sslscan $1 | tee recon/sslscan_$1_$port.txt"
-			echo "gobuster dir -w /usr/share/wordlists/dirb/common.txt -l -t 30 -e -k -x $pages -u https://$1:$port -o recon/gobuster_$1_$port.txt"
+			echo "gobuster dir -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -l -t 30 -e -k -x $pages -u https://$1:$port -o recon/gobuster_$1_$port.txt"
 			echo "nikto -host https://$1:$port -ssl | tee recon/nikto_$1_$port.txt"
 		else
-			echo "gobuster dir -w /usr/share/wordlists/dirb/common.txt -l -t 30 -e -k -x $pages -u http://$1:$port -o recon/gobuster_$1_$port.txt"
+			echo "gobuster dir -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -l -t 30 -e -k -x $pages -u http://$1:$port -o recon/gobuster_$1_$port.txt"
 			echo "nikto -host $1:$port | tee recon/nikto_$1_$port.txt"
 		fi
 		echo ""
